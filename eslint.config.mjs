da36cb2008782dcdaeb6ddb97f71655c9f4be6b5
reverse-ops <Tory.lil45@gmail.com>
2025-10-04 12:46:16 +0000

    CI workflow docker

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,21 @@
 import js from "@eslint/js";
-import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
-    files: ["src/**/*.js"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs", // ✅ pour autoriser require et module.exports
       globals: {
-        ...globals.node, // ✅ variables globales Node (module, require, process…)
+        require: "readonly",
+        module: "readonly",
+        console: "readonly",
       },
-    },
-  },
-  {
-    files: ["tests/**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
       sourceType: "commonjs",
-      globals: {
-        ...globals.jest, // ✅ autorise test, expect, describe…
-        ...globals.node,
-      },
     },
+    env: {
+      node: true,
+      es2021: true,
+      jest: true,
+    },
+    rules: {},
   },
 ];
